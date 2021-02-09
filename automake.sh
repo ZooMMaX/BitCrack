a1e1c7edef24a428e14f3dd2986817a5bd1d6d9e
ZooMMaX <zoommax2009@gmail.com>
2021-02-09 18:07:06 +0300

    Add files via upload

--- automake.sh
+++ automake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install gcc make
+apt install gcc make -y
 wget https://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
 wget https://ru.download.nvidia.com/XFree86/Linux-x86_64/450.102.04/NVIDIA-Linux-x86_64-450.102.04.run
 sh cuda_10.1.243_418.87.00_linux.run
