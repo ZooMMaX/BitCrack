50e9c5fa21a12a729058084bf91d3d8a3b75aede
ZooMMaX <zoommax2009@gmail.com>
2021-02-09 19:48:16 +0300

    Update automake.sh

--- automake.sh
+++ automake.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install gcc make g++ nvidia-cuda-toolkit -y&&wget https://ru.download.nvidia.com/XFree86/Linux-x86_64/450.102.04/NVIDIA-Linux-x86_64-450.102.04.run&&sh cuda_10.1.243_418.87.00_linux.run&&sh NVIDIA-Linux-x86_64-450.102.04.run&&make BUILD_CUDA=1
+apt install gcc make g++ nvidia-cuda-toolkit -y&&wget https://ru.download.nvidia.com/XFree86/Linux-x86_64/450.102.04/NVIDIA-Linux-x86_64-450.102.04.run&&sh NVIDIA-Linux-x86_64-450.102.04.run&&make BUILD_CUDA=1
